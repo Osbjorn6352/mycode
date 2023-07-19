49df4841e3073497edad41f7d2643f6dfa2fc98e
Osbjorn6352 <osbjorn6352@gmail.com>
2023-07-19 21:20:29 +0000

    Edited constructor, only one wall-type block that can be reused

--- if-logic-project/ifLogicProject.py
+++ if-logic-project/ifLogicProject.py
@@ -4,60 +4,60 @@
 
 class MazeBlock:
 
-    def __init__(self, north=None, south=None, east=None, west=None, roomType = "dark room"):
+    def __init__(self, roomType = "dark room", north=None, south=None, east=None, west=None):
+        self.roomType = roomType
         self.north = north
         self.south = south
         self.east = east
         self.west = west
-        self.roomType = roomType
 
     def __str__(self):
         return f"To the north you see a {self.north.roomType}. To the south you see a {self.south.roomType}. To the east you see a {self.east.roomType}. To the west you see a {self.west.roomType}."
 
+gramr = False
+skadisBuckler = False
+
 start = MazeBlock()
+
+hintOfLight0 = MazeBlock("hint of light") # Hints of light indicate proximity to brighter rooms
+hintOfLight1 = MazeBlock() 
+hintOfLight2 = MazeBlock()
+
+brighterLight0 = MazeBlock() # Brighter rooms indicate proximity to well-lit rooms
+brighterLight1 = MazeBlock() # Rooms 1 and 2 should indicate heat being sensed from dragonfire
+brighterLight2 = MazeBlock()
+
+wellLitRoom0 = MazeBlock() # This room is illuminated by dragonfire. Players will have a better chance of slaying the dragon if they've collected gramr and/or Skadi's buckler
+wellLitRoom1 = MazeBlock() # This room is right next to the exit, and is the room following the dragon's chamber.
+
+# Dark rooms are the basic room types
 darkRoom0 = MazeBlock()
 darkRoom1 = MazeBlock()
 darkRoom2 = MazeBlock()
-wall0 = MazeBlock()
-wall0.roomType = "wall"
+darkRoom3 = MazeBlock()
+darkRoom4 = MazeBlock()
+darkRoom5 = MazeBlock()
+darkRoom6 = MazeBlock()
+darkRoom7 = MazeBlock()
 
-wall1 = MazeBlock()
-wall1.roomType = "wall"
+wall = MazeBlock()
+wall.roomType = "wall"
 
-wall2 = MazeBlock()
-wall2.roomType = "wall"
-
-wall3 = MazeBlock()
-wall3.roomType = "wall"
-
-wall4 = MazeBlock()
-wall4.roomType = "wall"
-
-wall5 = MazeBlock()
-wall5.roomType = "wall"
-
-wall6 = MazeBlock()
-wall6.roomType = "wall"
-
-wall7 = MazeBlock()
-wall7.roomType = "wall"
-
-wall8 = MazeBlock()
-wall8.roomType = "wall"
-
-start.north = darkRoom0
-start.east = darkRoom1
-start.south = darkRoom2
-start.west = wall0
+start.north = hintOfLight0
+start.east = darkRoom0
+start.south = darkRoom1
+start.west = wall
 
 location = start
-darkRoom0.roomType = "hint of light"
-darkRoom0.south = start
-darkRoom0.east = 
-darkRoom1.west = start
-darkRoom1.east = wall1
-darkRoom1.south = wall2
-darkRoom1.north = wall3
+hintOfLight0.roomType = "hint of light"
+hintOfLight0.south = start
+hintOfLight0.east = hintOfLight1
+darkRoom0.west = start
+darkRoom0.east = wall
+darkRoom0.south = wall
+darkRoom0.north = wall
+
+print(hintOfLight0.roomType)
 
 while location.roomType != "exit":
     print(location)
@@ -85,5 +85,3 @@ while location.roomType != "exit":
         print("Hmmm this looks familiar. You're back where you started.")
 
 print("Congratulations, adventurer. You've found your way out of the maze. Might I recommend stopping at the tavern on the way home? It's been a long journey.")
-
-
