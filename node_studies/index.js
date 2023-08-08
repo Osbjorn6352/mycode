b401ac416fafcdfd9c221fdfcd9150956b69c555
Osbjorn6352 <osbjorn6352@gmail.com>
2023-08-08 19:38:14 +0000

    saving my work with node.js

---
+++ node_studies/index.js
@@ -0,0 +1,12 @@
+const axios = require('axios'); // this semicolon is optional 
+
+axios.get('http://api.open-notify.org/astros.json')
+
+    .then(response => {
+        console.log(response.data);
+    })
+
+    .catch(error => {
+        console.log(error);
+    });
+
